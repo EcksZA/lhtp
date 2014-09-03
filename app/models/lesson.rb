b53420a50a7e137222a72ad15deb95235dedd209
Xolani Mfuko <xmfuko@gmail.com>
2014-09-02 21:38:49 -0700

    return the lesson with the next-highest number than the current lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,9 @@
 class Lesson < ActiveRecord::Base
   validates :name, :content, presence: true
+
+  def next
+    lesson_sort = Lesson.all.sort_by(&:name)
+    current_index = lesson_sort.index(self)
+    @next_lesson = lesson_sort.fetch(current_index + 1)
+  end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
+require 'pry'
 
 describe Lesson do
   it { should validate_presence_of :name }
   it { should validate_presence_of :content }
+
+  context '#next' do
+    it 'returns the lesson with the next-highest number than the current lesson' do
+      current_lesson = Lesson.create({:name => 'lesson1', :content => 'blah', :number => 1})
+      next_lesson = Lesson.create({:name => 'lesson2', :content => 'blah', :number => 2})
+      expect(current_lesson.next).to eq next_lesson
+    end
+  end
 end
